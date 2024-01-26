0bcf463357f84cbc78731c71d19d009828e99412
dtovar <jtovar.ks2.tech>
2024-01-25 22:19:34 -0400

    Agregando el taildiwind a los titulos de formulario y el tidulo principal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header(){
 
     return(
-        <header className="grid">
-            <h1 className="uppercase">Desde Header Componente</h1>
-        </header>
+        <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+            Seguimiento Pacientes <span className="text-indigo-600">Veterinaria</span>
+        </h1>
     )
 }
 
